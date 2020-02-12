dc0c55e875fffddf9aa177a2f00012baa9acc8ba
GeorgeWL <george.langham@ivendi.com>
2020-02-12 12:03:49 +0000

    basic rich editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import EditorCreate from './EditorCreate';
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Markdown with Preview</h1>
+        </header>
+        <nav className="App-nav">
+          <ul>
+            <li>
+              <NavLink to="/" activeClassName="App-link">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/posts" activeClassName="App-link">
+                Posts
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/alerts" activeClassName="App-link">
+                Alerts
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <h1>Edit or Create Posts and Alerts</h1>
+            </Route>
+            <Route exact path={['/posts', '/alerts']}>
+              <EditorCreate />
+            </Route>
+            <Route path="/posts/:id">
+              <>placeholder posts id</>
+            </Route>
+            <Route path="/alerts/:id">
+              <>placeholder alerts id</>
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/EditorCreate.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import TextEditor from './Editor';
+function parseUrlHeader(url: string): string {
+  const isPost = /\/posts(\/{1}.*)*/.test(url);
+  const isAlert = /\/alerts(\/{1}.*)*/.test(url);
+  let header = '';
+  if (isPost) {
+    header = 'Posts';
+  }
+  if (isAlert) {
+    header = 'Alerts';
+  }
+  return header;
+}
+const EditorCreate: React.SFC = () => {
+  const { url } = useRouteMatch();
+  return (
+    <div>
+      <h1>Edit {parseUrlHeader(url)}</h1>
+      <TextEditor></TextEditor>
+    </div>
+  );
+};
+export default EditorCreate;
